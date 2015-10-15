45984898aabcbc549e6cb3c363e0caca8a724329
Anita Growsoon <anitagrowsoon@gmail.com>
2015-10-14 22:48:37 -0400

    created beta branch to test code, added deck, began char select

---
+++ deck.js
@@ -0,0 +1,69 @@
+//Deck of characters:
+//starting with 6 only!
+
+// var prototype: function(){
+//    this.name: name,
+//    this.avatar: 'square img url used for background',
+//    this.attack1: [attack1Name, a1damage(#1-5), a1type, 'img-src-url'], 'sound-src-url??'],
+//    this.attack2: [attack2Name, a12amage(#1-5), a2type, 'img-src-url'], 'sound-src-url??'],
+//    this.def: [defName, #1-5, 'img-src-url'], 'sound-src-url??'],
+//    this.accuracy: #1-10,
+// },
+
+
+pikachu: {
+   name: 'Pikachu',
+   avatar: ['http://www.pokemon-paradijs.com/animated-gifs/image/pikachu_rent2.gif', 'http://t02.deviantart.net/qIauUAAZi3LyEERUiqEMA0CKWN4=/fit-in/150x150/filters:no_upscale():origin()/pre15/a015/th/pre/f/2013/019/f/9/dead_pikachu_by_pickachua-d5s17w6.png', 'http://i1263.photobucket.com/albums/ii631/Pokemon-Vampire-Knight-lover/pikachu_.gif']
+   attack1: ['ElectroCute', 4.5, 'Electric', 'http://orig15.deviantart.net/e860/f/2010/105/8/e/pikachu___volt_tackle_by_hanshumon.gif'], //, 'sound-src-url??'],
+   attack2: ['Pound', 4, 'Physical', 'http://urpgdex.monbrey.com.au/art/models/25-2.gif'], //, 'sound-src-url??'],
+   def: ['Defibrilator', 3.5, 'http://i634.photobucket.com/albums/uu68/coocoo406/moving_pikachu.gif'], //, 'sound-src-url??'],
+   accuracy: 7,
+   //winMsg: ['http://img06.deviantart.net/6dae/i/2011/030/1/7/pikacide_by_kid_viral-d38fbjk.jpg', ('I don'+'t know where you got that axe, but you win buddy! An ELECTRIC victory!')]
+},
+
+sindel: {
+   name: 'Sindel',
+   avatar: ['http://vignette3.wikia.nocookie.net/mortalkombat/images/9/93/Vs_sindel.gif/revision/latest?cb=20090917142727&path-prefix=es', 'http://www.theageofmammals.com/blogmedia/mk/sindel-falling.gif', 'http://bestanimations.com/Games/Computer/MortalCombat/Sindel/mortalkombatsindelanimation-3.gif']
+   attack1: ['Scream', 4.5, 'Psychic', 'images/s-scream.gif'], //, 'sound-src-url??'],
+   attack2: ['Hair Whip', 4.5, 'Physical', 'images/s-whip.gif'], //, 'sound-src-url??'],
+   def: ['The Ring', 1.5, 'http://www.fightersgeneration.com/np2/char1/gifs/sindel-mk3-slip.gif'], //, 'sound-src-url??'],
+   accuracy: 8.5,
+},
+
+snorlax: {
+   name: 'Snorlax',
+   avatar: ['https://shadowdaleraven.files.wordpress.com/2013/03/snorlax.gif'],
+   attack1: ['Stomp', 5, 'Physical', 'img-src-url'], //, 'sound-src-url??'],
+   attack2: ['Break Wind', 100, 'Toxic', 'img-src-url'], //, 'sound-src-url??'],
+   def: ['Sleep', 3, 'img-src-url'], //, 'sound-src-url??'],
+   accuracy: 2,
+},
+
+kittyPatra: {
+   name: 'KittyPatra',
+   avatar: 'square img url used for background',
+   attack1: ['Sandstorm', 3, 'Ground', 'img-src-url'], //, 'sound-src-url??'],
+   attack2: ['Scratch', 2, 'Physical', 'img-src-url'], //, 'sound-src-url??'],
+   def: ['Mummify', 5, 'img-src-url'], //, 'sound-src-url??'],
+   accuracy: 9,
+},
+
+scorpion: {
+   name: 'Scorpion',
+   avatar: 'square img url used for background',
+   attack1: ['Spear', 4, 'Physical', 'img-src-url'], //, 'sound-src-url??'],
+   attack2: ['Hellfire Punch', 4.5, 'Fire', 'img-src-url'], //, 'sound-src-url??'],
+   def: ['Flips and Stuff', 3, 'img-src-url'], //, 'sound-src-url??'],
+   accuracy: 7,
+
+
+},
+
+rachel: {
+   name: 'Zevita',
+   avatar: 'square img url used for background',
+   attack1: ['Sing', 5, 'Psychic', 'img-src-url'], //, 'sound-src-url??'],
+   attack2: ['Taunt', 3, 'Psychic', 'img-src-url'], //, 'sound-src-url??'],
+   def: ['Dodge', 4, 'img-src-url'], //, 'sound-src-url??'],
+   accuracy: 8,
+},
--- pmk.js
+++ pmk.js
@@ -1,35 +1,45 @@
 window.onload = function () {
-    alert("It's loaded!");
+    console.log("It's loaded!");
+    //no idea what goes here yet lol
+    //global scorekeeping variables
 
-    var doc = document;
 
+    //to start game, remove header
     $(function() {
       var start = $("#start");
       var head = $("header");
       var headStuff = $("#list");
       var health = $(".health");
 
+      //when start button is clicked
       start.click(function() {
 
+         //erase the start button
+         start.detach();
+         //show character select screen
+         charSelectScreen();
+
+         //wait for character input to set width of health bars
+
+         //character input rec'd for both players
+
+         //coin toss determines who is player one
+         //set width of health bars based on health
+         //of chosen character for each player
+
+         //only the first time:
+         //roll back masthead,
         if(head.hasClass('open')) {
           head.removeClass('open');
           head.addClass('closed');
+
+          //show healthbars
           health.removeClass('closed');
           health.addClass('open');
           health.height('50px');
-          start.detach();
        }
 
-
-
-      //   } else {
-      //     head.removeClass('closed');
-      //     head.addClass('open');
-      //     head.height(true);
-      //     health.removeClass('open');
-      //     health.addClass('closed');
-      //     health.height(0);
-
+      //show game commands
 
 
       });
@@ -38,27 +48,39 @@ window.onload = function () {
 
 }
 
+var doc = document;
 
+var deck = {
+   pikachu: {
+      name: 'Pikachu',
+      avatar: ['http://www.pokemon-paradijs.com/animated-gifs/image/pikachu_rent2.gif', 'http://t02.deviantart.net/qIauUAAZi3LyEERUiqEMA0CKWN4=/fit-in/150x150/filters:no_upscale():origin()/pre15/a015/th/pre/f/2013/019/f/9/dead_pikachu_by_pickachua-d5s17w6.png', 'http://i1263.photobucket.com/albums/ii631/Pokemon-Vampire-Knight-lover/pikachu_.gif'],
+      attack1: ['ElectroCute', 4.5, 'Electric', 'http://orig15.deviantart.net/e860/f/2010/105/8/e/pikachu___volt_tackle_by_hanshumon.gif'], //, 'sound-src-url??'],
+      attack2: ['Pound', 4, 'Physical', 'http://urpgdex.monbrey.com.au/art/models/25-2.gif'], //, 'sound-src-url??'],
+      def: ['Defibrilator', 3.5, 'http://i634.photobucket.com/albums/uu68/coocoo406/moving_pikachu.gif'], //, 'sound-src-url??'],
+      accuracy: 7,
+      //winMsg: ['http://img06.deviantart.net/6dae/i/2011/030/1/7/pikacide_by_kid_viral-d38fbjk.jpg', ('I don'+'t know where you got that axe, but you win buddy! An ELECTRIC victory!')]
+   },
 
+   sindel: {
+      name: 'Sindel',
+      avatar: ['http://vignette3.wikia.nocookie.net/mortalkombat/images/9/93/Vs_sindel.gif/revision/latest?cb=20090917142727&path-prefix=es', 'http://www.theageofmammals.com/blogmedia/mk/sindel-falling.gif', 'http://bestanimations.com/Games/Computer/MortalCombat/Sindel/mortalkombatsindelanimation-3.gif'],
+      attack1: ['Scream', 4.5, 'Psychic', 'images/s-scream.gif'], //, 'sound-src-url??'],
+      attack2: ['Hair Whip', 4.5, 'Physical', 'images/s-whip.gif'], //, 'sound-src-url??'],
+      def: ['The Ring', 1.5, 'http://www.fightersgeneration.com/np2/char1/gifs/sindel-mk3-slip.gif'], //, 'sound-src-url??'],
+      accuracy: 8.5,
+   }
+};
 
+var charSelectScreen = function (){
+   var charSelect = doc.createElement('ul')
+   charSelect.id = "char-select-screen"
 
-//when page loads, assign click event to start button
-   //when start button is clicked
-   //player1 = 'true';
+   for (var i in deck) {
+      var char = doc.createElement('li');
+      char.setAttribute('class', 'char-avatars');
+      char.html = '<img src="' + deck[i].avatar[0] + '" class="char-avatar-imgs"></img>';
+      charSelect.appendChild(char);
+   }
+   $('#card-render').append(charSelect);
 
-   //display character array
-      //shuffle characters each time?
-
-//assign click event to character items
-   //click assigns that character to player
-   //click changes player1 to false (now player 2)
-
-
-//assign hover event to character items
-   //on hover, display stats
-
-
-// var characters = [array of character objects]
-//
-//
-// characterObjects:
+};
